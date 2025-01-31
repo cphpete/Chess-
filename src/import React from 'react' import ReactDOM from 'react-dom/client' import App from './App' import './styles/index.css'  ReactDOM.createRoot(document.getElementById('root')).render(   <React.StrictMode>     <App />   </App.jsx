e6932123badfc81f11b96bb189b3302e22beb7a1
cphpete <peter.scharling@gmail.com>
2025-01-31 01:15:48 +0100

    Update App.jsx

--- src/import React from 'react' import ReactDOM from 'react-dom/client' import App from './App' import './styles/index.css'  ReactDOM.createRoot(document.getElementById('root')).render(   <React.StrictMode>     <App />   </App.jsx
+++ src/import React from 'react' import ReactDOM from 'react-dom/client' import App from './App' import './styles/index.css'  ReactDOM.createRoot(document.getElementById('root')).render(   <React.StrictMode>     <App />   </App.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import ChessBoard from './components/ChessBoard'
 
+function App() {
+  return (
+    <div className="container">
+      <h1>Skak Slutspilstræner</h1>
+      <div className="app-container">
+        <ChessBoard />
+      </div>
+    </div>
+  )
+}
+
+export default App
